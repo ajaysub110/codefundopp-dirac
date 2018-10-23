f7bea86ddccc31b724157698c9b0296abfed3d34
Ajay Subramanian <ajaysubramanian110@gmail.com>
2018-10-24 00:09:11 +0530

    fixed upvote button and integrated with firebase

--- app/src/main/java/com/example/ajays/dirac/Forum/ForumAdapter.java
+++ app/src/main/java/com/example/ajays/dirac/Forum/ForumAdapter.java
@@ -2,8 +2,11 @@ package com.example.ajays.dirac.Forum;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,19 +15,29 @@ import android.widget.Toast;
 
 import com.example.ajays.dirac.Post.PostActivity;
 import com.example.ajays.dirac.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import static android.content.Context.MODE_PRIVATE;
 import static java.lang.Math.abs;
 
 
 public class ForumAdapter extends RecyclerView.Adapter<ForumViewHolder>{
     private ArrayList<ForumModel> forumModel;
     Context context;
+    FirebaseDatabase database;
+    SharedPreferences sp;
+    Integer current_upvotes;
 
     public ForumAdapter(Context context, ArrayList<ForumModel> forumModel) {
         this.forumModel = forumModel;
         this.context = context;
+        current_upvotes=0;
     }
 
     @Override
@@ -41,11 +54,39 @@ public class ForumAdapter extends RecyclerView.Adapter<ForumViewHolder>{
         holder.forum_post_description.setText(forumModel.get(i).getPost_description());
         holder.forum_post_num_upvotes.setText(forumModel.get(i).getNum_upvotes().toString());
 
+        database = FirebaseDatabase.getInstance();
+        sp = sp=context.getSharedPreferences("login", MODE_PRIVATE);
+
         holder.forum_post_upvote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                holder.forum_post_upvote.setColorFilter(context.getResources().getColor(R.color.colorSecondary));
                 // TODO: Increase upvote number in database
+                DatabaseReference data_ref = database.getReference("posts/"+ sp.getString("region",null)+'/');
+
+                data_ref.child(Integer.toString(i)).child("num_upvotes").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Log.d("data_read",dataSnapshot.getValue().toString());
+                        current_upvotes = Integer.parseInt(dataSnapshot.getValue().toString());
+                        Log.d("data_read",current_upvotes.toString());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                if (current_upvotes==0){
+                    data_ref.child(Integer.toString(i)).child("num_upvotes").setValue(current_upvotes+1);
+                    current_upvotes = 1;
+                    holder.forum_post_upvote.setColorFilter(context.getResources().getColor(R.color.colorSecondary));
+                }
+                else{
+                    data_ref.child(Integer.toString(i)).child("num_upvotes").setValue(current_upvotes-1);
+                    holder.forum_post_upvote.setColorFilter(context.getResources().getColor(R.color.black));
+                    current_upvotes = 0;
+                }
+                holder.forum_post_num_upvotes.setText(current_upvotes.toString());
             }
         });
 
